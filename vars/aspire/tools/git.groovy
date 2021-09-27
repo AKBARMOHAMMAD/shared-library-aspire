00556aefd338f6c2b507eee5f06e6a8ff449c3b4
Prasad Veginati <prasad.veginati@aspiresys.com>
2021-09-27 18:55:43 +0530

    initial commit

---
+++ vars/aspire/tools/git.groovy
@@ -0,0 +1,13 @@
+package aspire.tools
+
+def checkOut(REPO, BRANCH, REPO_CREDENTIALSID) {
+    script {
+        echo "checking out repo to $workspace"
+        echo "Branch is set to $BRANCH"
+        checkout([$class: 'GitSCM', branches: [[name: '*/'+BRANCH]], 
+        doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], 
+        userRemoteConfigs: [[credentialsId: REPO_CREDENTIALSID, url: REPO]]])
+    }
+}
+
+return this
