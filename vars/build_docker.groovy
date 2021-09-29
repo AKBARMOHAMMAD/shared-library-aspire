5cd0f32049141920987199663b3cd561910f43bf
Prasad Veginati <prasad.veginati@aspiresys.com>
2021-09-29 16:52:13 +0530

    changed code

--- vars/build_docker.groovy
+++ vars/build_docker.groovy
@@ -1,6 +1,6 @@
 def buildImage(APPNAME, DOCKER_REPO, dockerDir) {
     script {
-        docker.build("$DOCKER_REPO/"+"$APPNAME", "$dockerDir")
+        docker.build("$DOCKER_REPO", "$dockerDir")
     }
 
 }
--- vars/upload_dockerimage.groovy
+++ vars/upload_dockerimage.groovy
@@ -1,10 +1,10 @@
 def uploadImage(APPNAME, DOCKER_URL, DOCKER_REPO, REGISTRY_CREDS) {
         script {
             docker.withRegistry("$DOCKER_URL", REGISTRY_CREDS) {
-                docker.image("$DOCKER_REPO/" + "$APPNAME").push("${env.BUILD_NUMBER}")
+                docker.image("$DOCKER_REPO").push("${env.BUILD_NUMBER}")
                 //docker.image("$DOCKER_REPO/" + "$APPNAME").push("'${env.BUILD_NUMBER}'-'${tagVersion}'")
                 //updating "latest" is currently disabled due to MSO team now allowing service account to have overwrite permissions
-                docker.image("$DOCKER_REPO/" + "$APPNAME").push("latest")
+                docker.image("$DOCKER_REPO").push("latest")
             }
 
     }
